9b38c9cf297b3a18e7300705bb082c576b3c747b
kammy <kammy@kammy-HP-Pavilion-dv4-Notebook-PC>
2019-02-23 18:07:07 +0530

    update

--- Automation/src/main/java/com/me/AutomationDevelop/Automation/Core/OpenAndCloseBrowser.java
+++ Automation/src/main/java/com/me/AutomationDevelop/Automation/Core/OpenAndCloseBrowser.java
@@ -13,7 +13,7 @@ public class OpenAndCloseBrowser {
 	public void setUp() throws InterruptedException {
 		System.out.println("Stating........TestNGWith Parameter");
 		String path = System.getProperty("user.dir");
-		System.out.println("System.getProperty(\"user.dir\") :: "+ path+"/driver");
+		System.out.println("System.getProperty(\"user.dir\") :: "+ path+"/chromedriver");
 		System.setProperty("webdriver.chrome.driver", path+"/chromedriver");
 		String baseUrl = "https://www.amazon.in/";
 		Thread.sleep(5000);
