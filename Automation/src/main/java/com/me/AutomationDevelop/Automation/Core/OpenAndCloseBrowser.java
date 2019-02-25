f7576894f794c947cd863b34931210ca27dc81af
kammy <kammy@kammy-HP-Pavilion-dv4-Notebook-PC>
2019-02-25 23:28:32 +0530

    OACB

--- Automation/src/main/java/com/me/AutomationDevelop/Automation/Core/OpenAndCloseBrowser.java
+++ Automation/src/main/java/com/me/AutomationDevelop/Automation/Core/OpenAndCloseBrowser.java
@@ -1,7 +1,10 @@
 package com.me.AutomationDevelop.Automation.Core;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
@@ -13,11 +16,16 @@ public class OpenAndCloseBrowser {
 	public void setUp() throws InterruptedException {
 		System.out.println("Stating........TestNGWith Parameter");
 		String path = System.getProperty("user.dir");
-		System.out.println("System.getProperty(\"user.dir\") :: "+ path+"/Automation/chromedriver");
-		System.setProperty("webdriver.chrome.driver", path+"/chromedriver");
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setJavascriptEnabled(true);
+		System.out.println("System.getProperty(\"user.dir\") :: "+ path+"/chromedriver");
+		//System.setProperty("webdriver.chrome.driver", path+"/chromedriver");
 		String baseUrl = "https://www.amazon.in/";
 		Thread.sleep(5000);
-		driver = new ChromeDriver();
+		//driver = new ChromeDriver();
+		driver = new RemoteWebDriver(capabilities);
+		Capabilities actualCapabilities = ((RemoteWebDriver) driver).getCapabilities();
+		
 		System.out.println("Opned........");
 		Thread.sleep(5000);
 		driver.manage().window().maximize();
